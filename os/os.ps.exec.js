66ba104253da7a7ccc867284f801a0d40b1ec5a0
Edwin Young <edwin.w.young@gmail.com>
2016-03-25 18:48:41 -0400

    Fixed possible error in id assignment for processes

--- os/os.ps.exec.js
+++ os/os.ps.exec.js
@@ -3,8 +3,14 @@
     os._internals.ps.exec = initProcess;
     
     function initProcess(name, cb) {
+        // Takes care of possible duplicate IDs in the pcb queue
+        var max_id = -1;
+        for (var i = 0; i < os._internals.ps.pcb.length; i++) 
+            if (os._internals.ps.pcb[i].id > max_id)
+                max_id = os._internals.ps.pcb[i].id;
+
         os._internals.ps.pcb.push({
-            id: os._internals.ps.pcb.length,
+            id: max_id + 1,
             name: name,
             state: os._internals.ps.states.START,
             entryPoint: cb
